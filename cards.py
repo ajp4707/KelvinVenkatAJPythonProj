39c92d83600935d20fbb932a1653be49dbd8dfb8
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-08 22:30:05 -0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,1 +1,6 @@
-class cards:
+class Card():
+    def __init__ (self, val, suit, hid, loc):
+        self.value= int(val)
+        self.suit = suit
+        self.hidden= int(hid)
+        self.location=loc
