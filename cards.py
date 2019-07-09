f071f42b5a1248926d03dde0e05b282383c456eb
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2019-07-09 13:30:59 -0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,6 +1,11 @@
-class Card():
+import pygame
+
+class Card(pygame.sprite.Sprite):
     def __init__ (self, val, suit, hid, loc):
+        super(Card,self).__init__()
         self.value= int(val)
         self.suit = suit
         self.hidden= int(hid)
         self.location=loc
+        self.surf = pygame.surface.Surface((40,40))
+        self.image = pygame.image.load(f'{val}of{suit}.png').convert()
