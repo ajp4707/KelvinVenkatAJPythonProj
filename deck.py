26d33c87b846a25e515cdb2d68726377872ed84c
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-08 22:29:25 -0400

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,2 +1,28 @@
+import Card.py
+import random
 class Deck():
-  
+    def __init__(self, joker):
+        self.List=list()
+        self.Suits= ["clubs", "diamonds", "hearts", "spades"]
+        #Creates a standard 52 card deck
+        for suit in Suits:
+            for val in range(1,14):
+                self.List.append(card(val, suit, 0, "deck"))
+        #Jokers have the value 0 and the suit "joker"
+        if int(joker):
+            self.List.append(card(0, "joker", 0, "deck"))
+        self.shuffle()
+    #Shuffles the deck regardless of size
+    def shuffle(self):
+        size=len(self.List)
+        for i in range(0,500):
+            idx= random.randint(1,size-1)
+            self.List.append(self.List.pop(idx))
+    #Returns how ever many top cards and changes their location
+    def deal (self, num, loc):
+        temp=list()
+        for i in range(0,num):
+            card=self.List.pop()
+            card.location=loc
+            temp.append(card)
+        return temp
