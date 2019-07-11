4e0cb03207fb743a91695e62d9677b86df11625b
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2019-07-11 18:23:05 -0400

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,29 +1,50 @@
-import Card
-import random
+'''
+Created on Jul 9, 2019
 
-class Deck():
-    def __init__(self, joker):
-        self.List=list()
-        self.Suits= ["Clubs", "Diamonds", "Hearts", "Spades"]
-        #Creates a standard 52 card deck
-        for suit in self.Suits:
-            for val in range(1,14):
-                self.List.append(Card(val, suit, 0, "deck"))
-        #Jokers have the value 0 and the suit "joker"
-        if int(joker):
-            self.List.append(Card(0, "joker", 0, "deck"))
-        self.shuffle()
-    #Shuffles the deck regardless of size
-    def shuffle(self):
-        size=len(self.List)
-        for i in range(0,500):
-            idx= random.randint(1,size-1)
-            self.List.append(self.List.pop(idx))
-    #Returns how ever many top cards and changes their location
-    def deal (self, num, loc):
-        temp=list()
-        for i in range(0,num):
-            card=self.List.pop()
-            card.location=loc
-            temp.append(card)
-        return temp
+@author: ajp47
+'''
+import pygame
+
+class Card(pygame.sprite.Sprite):
+    CARD_WIDTH = 75
+    CARD_HEIGHT = 113
+    
+    def __init__ (self, val, suit, hid, x, y):
+        super(Card,self).__init__()
+        
+        self.value= int(val)
+        self.suit = suit
+        self.hidden= int(hid)
+        
+        self.name=""
+        self.setName()
+        self.image = pygame.transform.scale(pygame.image.load(f'cardimages/{self.name}{self.suit[0]}.png').convert(), (self.CARD_WIDTH, self.CARD_HEIGHT))
+        self.back = pygame.transform.scale(pygame.image.load(f'cardimages/blue_back.png').convert(), (self.CARD_WIDTH,self.CARD_HEIGHT))
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+        
+        self.dragging = False
+        
+    def getImage(self):
+        if self.hidden:
+            return self.back
+        else:
+            return self.image
+        
+    
+    
+    def toggleHide(self):
+        self.hidden = not self.hidden
+        
+    def setName(self):
+        if self.value == 1:
+            self.name = "A"
+        elif self.value == 11:
+            self.name = "J"
+        elif self.value == 12:
+            self.name = "Q"
+        elif self.value == 13:
+            self.name = "K"
+        else:
+            self.name = str(self.value)
