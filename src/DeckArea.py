adf6dceb16e3938d568ad6ee006c6a6cad56774b
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2019-07-14 20:21:56 -0400

    Add files via upload

---
+++ src/DeckArea.py
@@ -0,0 +1,23 @@
+from Hand import Hand
+class DeckArea(Hand):
+
+    def __init__(self, width, height, x, y, color, handlist):
+        super().__init__(width, height, x, y, color)
+        self.handList = handlist
+    
+    #n is number of cards to deal to each hand
+    def deal(self, n):
+        handnum = len(self.handList)
+        if len(self) < n*handnum:
+            print("Not enough cards!")
+            return
+        cardlist = self.sprites()
+        for i in range(0,n*handnum):
+            cardlist[i].rect.x = self.handList[i % handnum].x + 5 * i
+            cardlist[i].rect.y = self.handList[i % handnum].y + 5 
+        
+                
+                
+                
+            
+        
