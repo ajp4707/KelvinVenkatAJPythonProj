9c6a65dcbbe5f0ad511f75646c5dcd215f397673
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-15 20:21:58 -0400

    Update GUIrunner.py

--- src/GUIrunner.py
+++ src/GUIrunner.py
@@ -5,8 +5,7 @@ from Deck import *
 from Card import *
 from Hand import *
 from Middle import *
-from DeckArea import *
-
+from DeckArea import DeckArea
 #Screen and Hand Constants
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
@@ -32,94 +31,107 @@ ELEC_BLUE = (5, 142, 217)
 MOCCASIN = (244, 235, 217)
 GRULLO = (163, 154, 146)
 
-
-#initializes pygame module and window
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-
-#Creates middle area and hands. 
-middle = Middle(MIDDLE_WIDTH, MIDDLE_HEIGHT, SCREEN_WIDTH/2 - MIDDLE_WIDTH/2, SCREEN_HEIGHT/2 - MIDDLE_HEIGHT/2, ELEC_BLUE )
-hands = []
-hands.append(Hand(HORIZ_HAND_WIDTH, HORIZ_HAND_HEIGHT, int(SCREEN_WIDTH/2 - HORIZ_HAND_WIDTH/2), 625 - HORIZ_HAND_HEIGHT/2, MOCCASIN))
-hands.append(Hand(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, 1100 - VERT_HAND_WIDTH/2, int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), MOCCASIN))
-hands.append(Hand(HORIZ_HAND_WIDTH, HORIZ_HAND_HEIGHT, int(SCREEN_WIDTH/2 - HORIZ_HAND_WIDTH/2), 75 - HORIZ_HAND_HEIGHT/2, MOCCASIN))
-hands.append(Hand(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, 100 - VERT_HAND_WIDTH/2, int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), MOCCASIN))
-
-#initialize deck. In the future, determine whose turn it is?
-
-deck = Deck(0, 100 - Card.CARD_WIDTH/2, 75 - Card.CARD_HEIGHT/2)
-deckarea = DeckArea(VERT_HAND_WIDTH,HORIZ_HAND_HEIGHT, 100 - VERT_HAND_WIDTH/2, 75 - HORIZ_HAND_HEIGHT/2, GRULLO, hands)
-deckarea.update(deck)
-
-#clock controls FPS of game
-clock = pygame.time.Clock()
-#mouse vars are important for card motion
+#mouse variables
 mouse_x = 0
 mouse_y = 0
 
+#initializes pygame module and window
+def createScreen():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    middle = Middle(MIDDLE_WIDTH, MIDDLE_HEIGHT, SCREEN_WIDTH/2 - MIDDLE_WIDTH/2, SCREEN_HEIGHT/2 - MIDDLE_HEIGHT/2, ELEC_BLUE )
+    return middle,screen
+
+    #Creates middle area and hands. 
+def createHands(handNum):
+    hands = []
+    #1, 2, 3, and 4 are the only accepted arguments for the number of hands
+    hands.append(Hand(HORIZ_HAND_WIDTH, HORIZ_HAND_HEIGHT, int(SCREEN_WIDTH/2 - HORIZ_HAND_WIDTH/2), 625 - HORIZ_HAND_HEIGHT/2, MOCCASIN))
+    if (handNum==2):
+        hands.append(Hand(HORIZ_HAND_WIDTH, HORIZ_HAND_HEIGHT, int(SCREEN_WIDTH/2 - HORIZ_HAND_WIDTH/2), 75 - HORIZ_HAND_HEIGHT/2, MOCCASIN))
+    elif (handNum==3):
+        hands.append(Hand(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, 1100 - VERT_HAND_WIDTH/2, int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), MOCCASIN))
+        hands.append(Hand(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, 100 - VERT_HAND_WIDTH/2, int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), MOCCASIN))        
+    elif (handNum==4):
+        hands.append(Hand(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, 1100 - VERT_HAND_WIDTH/2, int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), MOCCASIN))
+        hands.append(Hand(HORIZ_HAND_WIDTH, HORIZ_HAND_HEIGHT, int(SCREEN_WIDTH/2 - HORIZ_HAND_WIDTH/2), 75 - HORIZ_HAND_HEIGHT/2, MOCCASIN))
+        hands.append(Hand(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, 100 - VERT_HAND_WIDTH/2, int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), MOCCASIN))
+    return hands
+#initialize Deck accepting parameters for whether you want jokers and the previously created hands. In the future, determine whose turn it is? 
+def makeDeck(joker,hands):
+    deck = Deck(joker, 100 - Card.CARD_WIDTH/2, 75 - Card.CARD_HEIGHT/2)
+    deckarea = DeckArea(VERT_HAND_WIDTH,HORIZ_HAND_HEIGHT, 100 - VERT_HAND_WIDTH/2, 75 - HORIZ_HAND_HEIGHT/2, GRULLO, hands)
+    deckarea.update(deck)
+    return deck,deckarea
+
 #Game loop - keeps the window open 
-running = True
-while running:
-    for event in pygame.event.get(): #checks the queue of events
-        if event.type == KEYDOWN:    #we can add many more commands here if we want
-            if event.key == K_ESCAPE:
+def runSim(numHands, joker):
+    clock = pygame.time.Clock()
+    middle,screen= createScreen()
+    hands= createHands(numHands)
+    deck,deckarea= makeDeck(joker, hands)
+    running = True
+    while running:
+        for event in pygame.event.get(): #checks the queue of events
+            if event.type == KEYDOWN:    #we can add many more commands here if we want
+                if event.key == K_ESCAPE:
+                    running = False
+                elif event.key == K_SPACE:
+                    deckarea.deal(7)
+                    print("check space")
+            elif event.type == QUIT:
                 running = False
-            elif event.key == K_SPACE:
-                deckarea.deal(7)
-                print("check space")
-        elif event.type == QUIT:
-            running = False
-        
-        #MOUSEBUTTONDOWN 3 is a right click, drags all of the cards. button 1 is a left click, drags one card
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 3:                   
-                for card in deck.List:
-                    if card.rect.collidepoint(event.pos):
-                        card.dragging = True
-                        mouse_x, mouse_y = event.pos
-                        offset_x = card.rect.x - mouse_x
-                        offset_y = card.rect.y - mouse_y
-            elif event.button == 1:
-                for i in range(len(deck.List)-1, -1, -1 ):
-                    if deck.List[i].rect.collidepoint(event.pos):
-                        deck.List[i].dragging = True
-                        mouse_x, mouse_y = event.pos
-                        offset_x = deck.List[i].rect.x - mouse_x
-                        offset_y = deck.List[i].rect.y - mouse_y
-                        deck.List.append(deck.List.pop(i)) #Put grabbed cards at the end of the deck, they will blit on top
-                        break
-        #For efficiency, Hands only check if cards have been added to them after MOUSEBUTTONUP 
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 3:
-                deck.undrag()
-            elif event.button == 1:
-                deck.undrag()
             
-            deck.hide()
-            deckarea.update(deck)
-            for hand in hands:
-                hand.update(deck)
-                hand.flip()
-            middle.update(deck)
-            middle.flip()
-            #if event.button == 2:
+            #MOUSEBUTTONDOWN 3 is a right click, drags all of the cards. button 1 is a left click, drags one Card
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 3:                   
+                    for card in deck.List:
+                        if card.rect.collidepoint(event.pos):
+                            card.dragging = True
+                            mouse_x, mouse_y = event.pos
+                            offset_x = card.rect.x - mouse_x
+                            offset_y = card.rect.y - mouse_y
+                elif event.button == 1:
+                    for i in range(len(deck.List)-1, -1, -1 ):
+                        if deck.List[i].rect.collidepoint(event.pos):
+                            deck.List[i].dragging = True
+                            mouse_x, mouse_y = event.pos
+                            offset_x = deck.List[i].rect.x - mouse_x
+                            offset_y = deck.List[i].rect.y - mouse_y
+                            deck.List.append(deck.List.pop(i)) #Put grabbed cards at the end of the Deck, they will blit on top
+                            break
+            #For efficiency, Hands only check if cards have been added to them after MOUSEBUTTONUP 
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 3:
+                    deck.undrag()
+                elif event.button == 1:
+                    deck.undrag()
                 
-                
-        elif event.type == pygame.MOUSEMOTION:
-            for card in deck.List:
-                if card.dragging:
-                    mouse_x, mouse_y = event.pos
-                    card.rect.x = mouse_x + offset_x
-                    card.rect.y = mouse_y + offset_y
-                
-    
-    screen.fill(DARK_PUCE)
-    screen.blit(middle.surf, (middle.x, middle.y))
-    screen.blit(deckarea.surf, (deckarea.x, deckarea.y))
-    for hand in hands:
-        screen.blit(hand.surf, (hand.x, hand.y))
-    for card in deck.List:
-        screen.blit(card.getImage(), (card.rect.x, card.rect.y))
-    pygame.display.flip() 
-    
-    clock.tick(30)
+                deck.hide()
+                deckarea.update(deck)
+                for hand in hands:
+                    hand.update(deck)
+                    hand.flip()
+                middle.update(deck)
+                middle.flip()
+                #if event.button == 2:
+                    
+                    
+            elif event.type == pygame.MOUSEMOTION:
+                for card in deck.List:
+                    if card.dragging:
+                        mouse_x, mouse_y = event.pos
+                        card.rect.x = mouse_x + offset_x
+                        card.rect.y = mouse_y + offset_y
+                    
+        
+        screen.fill(DARK_PUCE)
+        screen.blit(middle.surf, (middle.x, middle.y))
+        screen.blit(deckarea.surf, (deckarea.x, deckarea.y))
+        for hand in hands:
+            screen.blit(hand.surf, (hand.x, hand.y))
+        for card in deck.List:
+            screen.blit(card.getImage(), (card.rect.x, card.rect.y))
+        pygame.display.flip() 
+        
+        clock.tick(30)
