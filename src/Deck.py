d7dafde6f194e6b9d88d6c122ca946b7f8cf2bc3
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-17 16:48:44 -0400

    Update Deck.py

--- src/Deck.py
+++ src/Deck.py
@@ -1,10 +1,8 @@
-
 from Card import*
 import random
-
 #I think Deck should be an abstract representation of ALL cards
-#I propose a new class called DeckArea that extends Hand class, but is where all cards initially start
-#DeckArea would inherit the Group class, which allows iteration over all Sprites (cards) in the group
+#I propose a new class called deckArea that extends Hand class, but is where all cards initially start
+#deckArea would inherit the Group class, which allows iteration over all Sprites (cards) in the group
 class Deck():
     def __init__(self, joker, x, y):
         self.List=list()
@@ -12,10 +10,10 @@ class Deck():
         
         self.x = x 
         self.y = y
-        #Creates a standard 52 card deck
+        #Creates a standard 52 Card deck
         for suit in self.Suits:
             for val in range(1,14):
-                self.List.append(Card(val, suit, 1, x, y))
+                self.List.append(Card(val, suit, x, y))
         #Jokers have the value 0 and the suit "joker"
         if int(joker):
             self.addJoker(1, x, y)
@@ -34,14 +32,14 @@ class Deck():
             card.hidden = 1
     
     #NEEDS WORK. Move selected cards.rect to specific hand locations. Perhaps even stagger cards so you can see them all
-    #I propose removing this method from this class and adding it to DeckArea class.
+    #I propose removing this method from this class and adding it to deckArea class.
     '''
     def deal (self, num, x, y):
         for i in range(0, num):
             
-            card.rect.x = x 
-            card.rect.y = y
-            temp.append(card)
+            Card.rect.x = x 
+            Card.rect.y = y
+            temp.append(Card)
         return temp
     '''
     def undrag(self):
