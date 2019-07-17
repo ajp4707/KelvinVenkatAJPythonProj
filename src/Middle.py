486627765356cd1485859a062934b441f91f7b79
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-17 16:46:34 -0400

    Update Middle.py

--- src/Middle.py
+++ src/Middle.py
@@ -1,15 +1,11 @@
 import pygame
-from Card import *
-from Deck import *
+# from Card import *
+# from Deck import *
 from Hand import *
 
 
 class Middle(Hand):
     #Similar to hand properties, but cards in middle are all shown
     def __init__(self, width, height, x, y, color):
-        super().__init__(width, height, x, y, color)
-    
-    def flip(self):
-        for i in self:
-            i.hidden = 0
+        super().__init__(width, height, x, y, color,False)
             
