1709c9e4d6a559f9cd3390eb7d626a2b4d9f55fd
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-16 21:40:19 -0400

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -6,5 +6,8 @@ Created on Jul 15, 2019
 import GUIcreator
 #for testing
 
+#Color Schemes:
+#ScreenColor,MiddleColor,HandColor,Deck/DiscardColor
+#1: DARK_PUCE, ELEC_BLUE, MOCCASIN,GRULLO
 numhands, joker=4,0
-GUIcreator.runSim(numhands, joker)
+GUIcreator.runSim(numhands,1, joker)
