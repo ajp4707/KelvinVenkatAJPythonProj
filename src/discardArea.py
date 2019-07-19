d5e54b088fb8d0fc84b44bade4120d4b9af92ea5
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-19 00:18:20 -0400

    Update discardArea.py

--- src/discardArea.py
+++ src/discardArea.py
@@ -1,11 +1,12 @@
 import pygame
 # from Card import *
 # from Deck import *
-from Hand import *
+from Area import *
 
+HORIZ_HAND_WIDTH, HORIZ_HAND_HEIGHT = 800, 133
+VERT_HAND_WIDTH, VERT_HAND_HEIGHT = 176, 400
 
-class discardArea(Hand):
+class discardArea(Area):
     #Similar to hand properties, but cards in middle are all shown
-    def __init__(self, width, height, x, y, color):
-        super().__init__(width, height, x, y, color,False)
-            
+    def __init__(self, color):
+        super().__init__(VERT_HAND_WIDTH,HORIZ_HAND_HEIGHT, 1100 - VERT_HAND_WIDTH/2, 625 - HORIZ_HAND_HEIGHT/2, color,False)
