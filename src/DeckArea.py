3be1043cf476b8d75e44ee78b53f3ba776b4f268
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-19 00:15:28 -0400

    Update DeckArea.py

--- src/DeckArea.py
+++ src/DeckArea.py
@@ -1,8 +1,12 @@
-import Hand
-class deckArea(Hand.Hand):
+import Area
 
-    def __init__(self, width, height, x, y, color, handlist):
-        super().__init__(width, height, x, y, color,True)
+HORIZ_HAND_WIDTH, HORIZ_HAND_HEIGHT = 800, 133
+VERT_HAND_WIDTH, VERT_HAND_HEIGHT = 176, 400
+
+class deckArea(Area.Area):
+
+    def __init__(self, color, handlist):
+        super().__init__(VERT_HAND_WIDTH,HORIZ_HAND_HEIGHT, 100 - VERT_HAND_WIDTH/2, 75 - HORIZ_HAND_HEIGHT/2, color,True)
         self.handList = handlist
     #n is number of cards to deal to each hand
     def deal(self, n):
@@ -14,9 +18,3 @@ class deckArea(Hand.Hand):
         for i in range(0,n*handnum):
             cardlist[i].rect.x = self.handList[i % handnum].x + 5 * i
             cardlist[i].rect.y = self.handList[i % handnum].y + 5 
-        
-                
-                
-                
-            
-        
