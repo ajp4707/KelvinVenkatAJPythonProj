3211e6cb47e188035a275ff1509ce5bc8311b5c9
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-19 00:18:51 -0400

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -7,8 +7,12 @@ import GUIcreator
 #for testing
 
 #Color Schemes:
-#ScreenColor,MiddleColor,HandColor,Deck, DiscardColor
-#1: DARK_PUCE, ELEC_BLUE, MOCCASIN,GRULLO, DARK_RED
+#   ScreenColor,  MiddleColor, HandColor,   Deck,   DiscardColor
+#1: DARK_PUCE,    ELEC_BLUE,   MOCCASIN,    GRULLO, DARK_RED
+#2: BLACK,        MOCCASIN,    GRULLO,      GREEN,  DARK_RED
+#3: MIDNIGHT_BLUE,SILVER,      LIGHT_GRAY,  GREEN,  VIOLET_RED
+#4: DARK_BLUE,    HONEYDEW,    SPRING_GREEN,BEIGE,  TEAL
+
 numhands, joker=4,0
-gui=GUIcreator.GUI(numhands,1, joker)
+gui=GUIcreator.GUI(numhands,4, joker)
 gui.runSim()
