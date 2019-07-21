d77897abb57ae3a6d39f5326f32652462f8198ac
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-21 18:23:41 -0400

    Update VerticalHand.py

--- src/VerticalHand.py
+++ src/VerticalHand.py
@@ -1,17 +1,36 @@
-'''
+'''     /
 Created on Jul 18, 2019
 
 @author: Kel N
 '''
-from Area import Area
+from Area import *
 
 SCREEN_WIDTH, SCREEN_HEIGHT= 1200, 700
 VERT_HAND_WIDTH, VERT_HAND_HEIGHT = 176, 400
 
 class verticalHand(Area):
     #An area in which all cards are shown
-    def __init__(self, color, location):
+    def __init__(self, color, param,location):
         if location=="Left":
-            super().__init__(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, 100 - VERT_HAND_WIDTH/2, int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), color,False)
+            self.handx=100
         else:
-            super().__init__(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, 1100 - VERT_HAND_WIDTH/2, int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), color,False)
+            self.handx=1100
+        super().__init__(VERT_HAND_WIDTH, VERT_HAND_HEIGHT, self.handx - VERT_HAND_WIDTH/2, 
+                         int(SCREEN_HEIGHT/2 - VERT_HAND_HEIGHT/2), color, False)
+        self.sortParam=param
+
+#https://docs.python.org/3/howto/sorting.html- Refreshed on how to sort with lambda functions
+    def display(self):
+        if self.sortParam == "Suit":
+            temp=sorted(self,key=Card.getValue)
+            newOrder=sorted(temp,key=Card.compareSuit)
+        else:
+            temp=sorted(self,key=Card.compareSuit)
+            newOrder=sorted(temp,key=Card.getValue)
+        size=len(newOrder)
+        if size !=0:
+            step=250/size
+        count=0
+        for card in newOrder:
+            card.rect.x, card.rect.y=self.handx - Card.CARD_WIDTH/2,225 -Card.CARD_HEIGHT/2+step*count
+            count+=1
