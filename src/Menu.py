a77fdf224e2ff21c1c5cdcf1e8c9acc09bb27b48
ChiliPaneer <33398322+ChiliPaneer@users.noreply.github.com>
2019-07-21 13:50:34 -0400

    Update Menu.py

--- src/Menu.py
+++ src/Menu.py
@@ -2,59 +2,73 @@
 import sys
 import pygame
 from pygame.locals import*
-from Deck import *
+"""from Deck import *
 from Card import *
 from VerticalHand import *
 from HorizontalHand import horizontalHand
 from Middle import *
 from DeckArea import *
-from discardArea import discardArea
+from discardArea import discardArea"""
 
 
-class Menu:
-    # Screen and Hand Constants
-    SCREEN_WIDTH = 1200
-    SCREEN_HEIGHT = 700 
-
-    def text_objects(text, font):
-        textSurface = font.render(text, True, (20,255,150))
-        return textSurface, textSurface.get_rect()
-
-    # initializes pygame module and window
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    largeText = pygame.font.Font('freesansbold.ttf', 80)
-    TextSurf, TextRect = text_objects("Welcome to Card Simulator", largeText)
+class Menu(object):
+    def __init__(self):
+        # Screen and Hand Constants
+        self.SCREEN_WIDTH = 1200
+        self.SCREEN_HEIGHT = 700
+        # initializes pygame module and window
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.largeText = pygame.font.Font('freesansbold.ttf', 80)
+        self.smallText = pygame.font.Font('freesansbold.ttf', 20)
+        self.TextSurf, self.TextRect = self.text_objects("Welcome to Card Simulator", self.largeText)
 
 
-    def button(self, x, y, w, h, ic, ac, func):
+    def text_objects(self, text, font):
+        self.textSurface = font.render(text, True, (20,255,150))
+        return self.textSurface, self.textSurface.get_rect()
+
+
+
+
+
+
+    def button(self, x, y, w, h, ic, ac, im, am):
         mouse = pygame.mouse.get_pos()
         if x < mouse[0] < x+w and y < mouse[1] < y+h:  #checks if mouse is over button, then changes color to active color
-            pygame.draw.rect(gameDisplay, ac, (x,y,w,h))
+            pygame.draw.rect(self.screen, ac, (x,y,w,h))
+            msg = am
         else:
-            pygame.draw.rect(gameDisplay, ic, (w,y,w,h)) #draws default button parameters
+            pygame.draw.rect(self.screen, ic, (x,y,w,h)) #draws default button parameters
+            msg = im
+        self.TextSurf, self.TextRect = self.text_objects(msg, self.smallText)
+        self.TextRect.center = ((x + (w / 2)), (y + (h / 2)))
+        self.screen.blit(self.TextSurf, self.TextRect)
 
 
 
+    def runMenu(self):
+        running = True
+        while running:
+            clock = pygame.time.Clock()
+            for event in pygame.event.get():  # checks the queue of events
+                print(event)
+                if event.type == KEYDOWN:  # we can add many more commands here if we want
+                    if event.key == K_ESCAPE:
+                        running = False
+            self.screen.fill((255,255,255))
 
-    running = True
-    while running:
-        clock = pygame.time.Clock()
-        for event in pygame.event.get():  # checks the queue of events
-            print(event)
-            if event.type == KEYDOWN:  # we can add many more commands here if we want
-                if event.key == K_ESCAPE:
-                    running = False
-        screen.fill((255,255,255))
+            self.TextRect.center = ((self.SCREEN_WIDTH / 2), (self.SCREEN_HEIGHT / 2))
+            self.screen.blit(self.TextSurf, self.TextRect)
 
-        TextRect.center = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
-        screen.blit(TextSurf, TextRect)
-
-        pygame.draw.rect(screen, (0,255,0), (200, 450, 100, 50))
-        pygame.draw.rect(screen, (255,0,0), (900, 450, 100, 50))
-
-        pygame.display.flip()
-
-        clock.tick(30)
+            #pygame.draw.rect(screen, (0,255,0), (200, 450, 100, 50))
+            pygame.draw.rect(self.screen, (255,0,0), (900, 450, 100, 50))
+            self.button(200, 450, 100, 50, (0,255,0), (255,0,0), "inactive", "active",)
+            pygame.display.flip()
 
 
+            clock.tick(30)
+
+
+a = Menu()
+a.runMenu()
