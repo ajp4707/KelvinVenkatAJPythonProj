f99a9e509440bfd72510d1774d2c3e4595ba84fb
ChiliPaneer <33398322+ChiliPaneer@users.noreply.github.com>
2019-07-21 15:32:55 -0400

    Added text and function button.

--- src/Menu.py
+++ src/Menu.py
@@ -10,6 +10,35 @@ from Middle import *
 from DeckArea import *
 from discardArea import discardArea"""
 
+#colors and list form GUI creator
+#Color Constants
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+DARK_RED = (232, 23, 23)
+LIME = (0,255,0)
+BLUE = (0,0, 255)
+DARK_PUCE = (72, 61, 63)
+ELEC_BLUE = (5, 142, 217)
+MOCCASIN = (244, 235, 217)
+GRULLO = (163, 154, 146)
+DARK_MAGENTA = (139,0,139)
+BEIGE=(245,245,220)
+LIGHT_GRAY=(119,136,153)
+GREEN=(0,128,0)
+SILVER=(192,192,192)
+DARK_BLUE=(0,0,139)
+MIDNIGHT_BLUE=(25,25,112)
+VIOLET_RED=(219,112,147)
+HONEYDEW=(240,255,240)
+TEAL=(0,128,128)
+SPRING_GREEN=(0,250,154)
+
+#Color Schemes
+colorSchemes=[[DARK_PUCE, ELEC_BLUE,MOCCASIN, GRULLO, DARK_RED],
+              [BLACK, MOCCASIN, GRULLO, GREEN, DARK_RED],
+              [MIDNIGHT_BLUE,SILVER,LIGHT_GRAY, GREEN, VIOLET_RED ],
+              [DARK_BLUE,HONEYDEW,SPRING_GREEN, BEIGE, TEAL]]
 
 class Menu(object):
     def __init__(self):
@@ -25,27 +54,26 @@ class Menu(object):
 
 
     def text_objects(self, text, font):
-        self.textSurface = font.render(text, True, (20,255,150))
+        self.textSurface = font.render(text, True, (0,0,0))
         return self.textSurface, self.textSurface.get_rect()
 
-
-
+    def dispText(self,msg,x,y,w,h):
+        self.TextSurf, self.TextRect = self.text_objects(msg, self.smallText)
+        self.TextRect.center = ((x + (w / 2)), (y + (h / 2)))
+        self.screen.blit(self.TextSurf, self.TextRect)
 
 
 
     def button(self, x, y, w, h, ic, ac, im, am):
         mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
         if x < mouse[0] < x+w and y < mouse[1] < y+h:  #checks if mouse is over button, then changes color to active color
             pygame.draw.rect(self.screen, ac, (x,y,w,h))
             msg = am
         else:
             pygame.draw.rect(self.screen, ic, (x,y,w,h)) #draws default button parameters
             msg = im
-        self.TextSurf, self.TextRect = self.text_objects(msg, self.smallText)
-        self.TextRect.center = ((x + (w / 2)), (y + (h / 2)))
-        self.screen.blit(self.TextSurf, self.TextRect)
-
-
+        self.dispText(msg, x, y, w, h)
 
     def runMenu(self):
         running = True
@@ -58,12 +86,11 @@ class Menu(object):
                         running = False
             self.screen.fill((255,255,255))
 
-            self.TextRect.center = ((self.SCREEN_WIDTH / 2), (self.SCREEN_HEIGHT / 2))
+
             self.screen.blit(self.TextSurf, self.TextRect)
 
             #pygame.draw.rect(screen, (0,255,0), (200, 450, 100, 50))
-            pygame.draw.rect(self.screen, (255,0,0), (900, 450, 100, 50))
-            self.button(200, 450, 100, 50, (0,255,0), (255,0,0), "inactive", "active",)
+            self.button(200, 450, 100, 50, RED , GREEN , "inactive", "active",)
             pygame.display.flip()
 
 
