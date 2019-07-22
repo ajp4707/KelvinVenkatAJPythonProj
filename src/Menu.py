bc5d83150ced11574827dac699364137deedba57
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-21 22:48:08 -0400

    Removed unused colorschemes and some whitespace

--- src/Menu.py
+++ src/Menu.py
@@ -1,10 +1,8 @@
-# code learned from pythonprogramming.net pygame tutorials
-# @author Venkat Pamulapati
+#Venkat - citing code from pythonprogramming.net
 import sys
 import pygame
 from pygame.locals import*
 from Toggle import *
-
 
 #colors and list form GUI creator
 #Color Constants
@@ -31,14 +29,6 @@ TEAL=(0,128,128)
 SPRING_GREEN=(0,250,154)
 labelFont = BLACK
 
-#Color Schemes
-colorSchemes=[[DARK_PUCE, ELEC_BLUE,MOCCASIN, GRULLO, DARK_RED],
-              [BLACK, MOCCASIN, GRULLO, GREEN, DARK_RED],
-              [MIDNIGHT_BLUE,SILVER,LIGHT_GRAY, GREEN, VIOLET_RED ],
-              [DARK_BLUE,HONEYDEW,SPRING_GREEN, BEIGE, TEAL]]
-
-
-
 class Menu(object):
     def __init__(self):
         # Screen and Hand Constants
@@ -60,7 +50,6 @@ class Menu(object):
     scend = Toggle(800, 575, 200, 50, ["Ascending", "Descending"])
     begin = Toggle(300, 640, 600, 50, ["Begin"])
     listA = [joker, numHands, colorScheme, sortHands, scend, begin]
-
 
     def text_objects(self, text, font, color):
         self.textSurface = font.render(text, True, color)
@@ -88,25 +77,19 @@ class Menu(object):
         if toggle.x < mouse[0] < toggle.x + toggle.w and toggle.y < mouse[1] < toggle.y + toggle.h or toggle.attr % len(toggle.mod) != 0:  # checks if mouse is over button, then changes color to active color
             pygame.draw.rect(self.screen, ELEC_BLUE, (toggle.x, toggle.y, toggle.w, toggle.h))
             msg = toggle.mod[toggle.attr%(len(toggle.mod))]
-
         #default option
         else:
             pygame.draw.rect(self.screen, SILVER, (toggle.x, toggle.y, toggle.w, toggle.h))  # draws default button parameters
             msg = toggle.mod[0]
-
-
         self.dispText(msg, toggle.x, toggle.y, toggle.w, toggle.h, self.buttonFont, BLACK)
-
     #runs game loop and menu
     def runMenu(self, list):
         running = True
         clock = pygame.time.Clock()
 
         while running:
-
             self.screen.fill(MOCCASIN)
             self.drawLabels()
-
             for event in pygame.event.get():  # checks the queue of events
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
@@ -118,10 +101,8 @@ class Menu(object):
                                 i.attr += 1
                 elif event.type == QUIT:
                     running = False
-
             for i in range(len(list)):
                 self.drawButton(list[i])
-
             #checks if begin was selected, then closes gui and returns values
             if list[5].attr > 0:
                 pygame.quit()
@@ -138,12 +119,7 @@ class Menu(object):
              #print(self.toggle1.mod[self.toggle1.attr%2])
             #self.dispText("Do you want jokers", 200, 125, 200, 200)
             pygame.display.flip()
-
             clock.tick(60)
-
-
 
 c = Menu()
 c.runMenu(c.listA)
-
-
