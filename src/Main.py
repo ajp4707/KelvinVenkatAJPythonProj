20b7754dbcc340007326931405f275c301157cc1
9KelvinN <49849026+9KelvinN@users.noreply.github.com>
2019-07-21 23:51:33 -0400

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -15,13 +15,11 @@ from Menu import*
 #Joker denotes whether Jokes are present or not
 menu = Menu()
 params = menu.runMenu(menu.listA)
-print(params)
 joker = params[0]
 numhands = params[1]+1
 colorScheme = params[2]
 sortHands = params[3]
 sortOrder = params[4]
 #SortParams denotes whether how you want to sort each hand. The first hand is the bottom hand and the other numbers are given in a counter-clockwise rotations
-sortParams= [("Value",False),("Value",False),("Value",True),("Value",True)]
-gui=GUIcreator.GUI(numhands,colorScheme, joker, sortParams)
+gui=GUIcreator.GUI(numhands,colorScheme, joker, (sortHands,sortOrder))
 gui.runSim()
