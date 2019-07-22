ffcf207347b934092a9020497ef0cb31124fbd78
ChiliPaneer <33398322+ChiliPaneer@users.noreply.github.com>
2019-07-21 21:53:33 -0400

    Main now runs menu and passes the parameters.

--- src/Main.py
+++ src/Main.py
@@ -4,6 +4,7 @@ Created on Jul 15, 2019
 @author: Kel N
 '''
 import GUIcreator
+from Menu import*
 #Color Schemes:
 #   ScreenColor,  MiddleColor, HandColor,   Deck,   DiscardColor
 #1: DARK_PUCE,    ELEC_BLUE,   MOCCASIN,    GRULLO, DARK_RED
@@ -12,7 +13,14 @@ import GUIcreator
 #4: DARK_BLUE,    HONEYDEW,    SPRING_GREEN,BEIGE,  TEAL
 
 #Joker denotes whether Jokes are present or not
-numhands, colorScheme, joker=4,4,True
+menu = Menu()
+params = menu.runMenu(menu.listA)
+print(params)
+joker = params[0]
+numhands = params[1]
+colorScheme = params[2]
+sortHands = params[3]
+sortOrder = params[4]
 #SortParams denotes whether how you want to sort each hand. The first hand is the bottom hand and the other numbers are given in a counter-clockwise rotations
 sortParams= [("Value",False),("Value",False),("Value",True),("Value",True)]
 gui=GUIcreator.GUI(numhands,colorScheme, joker, sortParams)
