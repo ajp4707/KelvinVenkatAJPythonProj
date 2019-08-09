05055e26378c758615fc37c53ec4d185d48e1e40
Benjamin Wheeler <benjaminwheeler@ufl.edu>
2019-08-08 23:21:30 -0400

    Cleaned comments

--- src/Main.py
+++ src/Main.py
@@ -5,18 +5,9 @@ Created on Jul 15, 2019
 '''
 import GUIcreator
 from Menu import *
-# Color Schemes:
-#   ScreenColor,  MiddleColor, HandColor,   Deck,   DiscardColor
-# 1: DARK_PUCE,    ELEC_BLUE,   MOCCASIN,    GRULLO, DARK_RED
-# 2: BLACK,        MOCCASIN,    GRULLO,      GREEN,  DARK_RED
-# 3: MIDNIGHT_BLUE,SILVER,      LIGHT_GRAY,  GREEN,  VIOLET_RED
-# 4: DARK_BLUE,    HONEYDEW,    SPRING_GREEN,BEIGE,  TEAL
 
-# Joker denotes whether Jokes are present or not
 menu = Menu()
 params = menu.run_menu()
 
-# SortParams denotes whether how you want to sort each hand.
-# The first hand is the bottom hand and the other numbers are given in a counter-clockwise rotations
-gui = GUIcreator.GUI(params)
-gui.run_sim()
+game = GUIcreator.GUI(params)
+game.run_sim()
